b52b608178cd259a3624e3000c8c52bd60534730
yurii <yurii.androsiuk@gmail.com>
2020-02-16 01:18:15 +0200

    test

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+	let sum = 0;
+	for (let i = 0; i < matrix.length; i++) {
+		for (let z = 0; z < matrix[i].length; z++) {
+			if (matrix[i][z] == '^^') {
+				sum += 1;
+			}
+		}
+	}
+	return sum;
 };
+
+
