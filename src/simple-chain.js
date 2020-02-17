ba0bc93f7e082c7d14a4c90dd03591913111d53a
yurii <yurii.androsiuk@gmail.com>
2020-02-17 03:45:08 +0200

    feat: transform array, simple-chain and hanoi-tower

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,45 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+	chain: [],
+	chainError() {
+		this.chain = [];
+		throw new Error();
+	},
+	getLength() {
+		return this.chain.length;
+	},
+	addLink(value) {
+		this.chain.push(value);
+		return this;
+	},
+	removeLink(position) {
+		if (typeof (position) === 'number') {
+			position = position - 1;
+			if (position < 0 || position >= this.getLength()) {
+				this.chainError();
+			}
+
+			this.chain.splice(position, 1);
+			return this;
+
+		} else {
+			this.chainError();
+		}
+	},
+	reverseChain() {
+		this.chain.reverse();
+		return this;
+	},
+	finishChain() {
+		let chain = '';
+
+		for (let i = 0; i < this.getLength(); i++) {
+			chain += `( ${this.chain[i]} )`;
+			i < this.getLength() - 1 ? chain += '~~' : '';
+		}
+
+		this.chain = [];
+		return chain;
+	}
 };
 
 module.exports = chainMaker;
