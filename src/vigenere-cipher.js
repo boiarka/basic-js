ddf9b730301149438625e1001c7f860c92c13383
Yurii <yurii.androsiuk@gmail.com>
2020-02-19 01:22:24 +0200

    feat: vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,54 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(isRev) {
+        if (typeof isRev == 'undefined') {
+            this.isRev = true;
+        } else {
+            this.isRev = isRev;
+        }
+    }
+    encrypt(m, k) {
+        m = m.toUpperCase();
+        k = k.toUpperCase();
+        let res = '';
+        let moduleN = '';
+        let j = 0;
+        for (let i = 0; i < m.length; i++) {
+            if (/[A-Z]/.test(m[i])) {
+                let mesNum = m.charCodeAt(i) - 65;
+                let keyNum = k.charCodeAt(j) - 65;
+                let sum = mesNum + keyNum;
+                moduleN = sum >= 26 ? sum - 26 : sum;
+                res += String.fromCharCode(moduleN + 65);
+                j === k.length - 1 ? j = 0 : j++;
+            } else {
+                res += m[i];
+            }
+        }
+
+        return this.isRev ? res : res.split('').reverse().join('');
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(m, k) {
+        m = m.toUpperCase();
+        k = k.toUpperCase();
+        let res = '';
+        let moduleN = '';
+        let j = 0;
+        for (let i = 0; i < m.length; i++) {
+            if (/[A-Z]/.test(m[i])) {
+                let mesNum = m.charCodeAt(i) - 65;
+                let keyNum = k.charCodeAt(j) - 65;
+                let sum = mesNum + 26 - keyNum;
+                moduleN = sum >= 26 ? sum - 26 : sum;
+                moduleN < 0 ? moduleN = moduleN + 26 : false;
+                res += String.fromCharCode(moduleN + 65);
+                j === k.length - 1 ? j = 0 : j++;
+            } else {
+                res += m[i];
+            }
+        }
+
+        return this.isRev ? res : res.split('').reverse().join('');
     }
 }
 
