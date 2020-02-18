797eb7ce69df47168383f2f3a21daaaa67b91d60
Yurii <yurii.androsiuk@gmail.com>
2020-02-19 01:52:49 +0200

    refactor: vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,34 +1,20 @@
 class VigenereCipheringMachine {
     constructor(isRev) {
-        if (typeof isRev == 'undefined') {
-            this.isRev = true;
-        } else {
-            this.isRev = isRev;
-        }
+        this.isRev = isRev != undefined ? isRev : true;
     }
     encrypt(m, k) {
-        m = m.toUpperCase();
-        k = k.toUpperCase();
-        let res = '';
-        let moduleN = '';
-        let j = 0;
-        for (let i = 0; i < m.length; i++) {
-            if (/[A-Z]/.test(m[i])) {
-                let mesNum = m.charCodeAt(i) - 65;
-                let keyNum = k.charCodeAt(j) - 65;
-                let sum = mesNum + keyNum;
-                moduleN = sum >= 26 ? sum - 26 : sum;
-                res += String.fromCharCode(moduleN + 65);
-                j === k.length - 1 ? j = 0 : j++;
-            } else {
-                res += m[i];
-            }
-        }
-
-        return this.isRev ? res : res.split('').reverse().join('');
+        let res = this.crypt(m, k, true);
+        this.isRev ? res : res = res.split('').reverse().join('');
+        return res;
     }
 
     decrypt(m, k) {
+        let res = this.crypt(m, k, false);
+        this.isRev ? res : res = res.split('').reverse().join('');
+        return res;
+    }
+
+    crypt(m, k, enc) {
         m = m.toUpperCase();
         k = k.toUpperCase();
         let res = '';
@@ -38,9 +24,15 @@ class VigenereCipheringMachine {
             if (/[A-Z]/.test(m[i])) {
                 let mesNum = m.charCodeAt(i) - 65;
                 let keyNum = k.charCodeAt(j) - 65;
-                let sum = mesNum + 26 - keyNum;
-                moduleN = sum >= 26 ? sum - 26 : sum;
-                moduleN < 0 ? moduleN = moduleN + 26 : false;
+
+                if (enc) {
+                    let sum = mesNum + keyNum;
+                    moduleN = sum >= 26 ? sum - 26 : sum;
+                } else {
+                    let sum = mesNum + 26 - keyNum;
+                    moduleN = sum >= 26 ? sum - 26 : sum;
+                    moduleN < 0 ? moduleN = moduleN + 26 : false;
+                }
                 res += String.fromCharCode(moduleN + 65);
                 j === k.length - 1 ? j = 0 : j++;
             } else {
@@ -48,8 +40,10 @@ class VigenereCipheringMachine {
             }
         }
 
-        return this.isRev ? res : res.split('').reverse().join('');
+        return res;
     }
+
+
 }
 
 module.exports = VigenereCipheringMachine;
